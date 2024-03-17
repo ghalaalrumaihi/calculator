5184ca70db49c6538cb8f4569bbe0704846bd5ed
f-altememee <faltememee.w@gmail.com>
2024-03-17 10:09:42 +0300

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by nbk on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
