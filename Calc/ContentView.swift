a573c62fe07bde3c73f3224b123e9b53ad9e1476
f-altememee <faltememee.w@gmail.com>
2024-03-18 10:53:55 +0300

    first commit

--- Calc/ContentView.swift
+++ Calc/ContentView.swift
@@ -1,21 +1,15 @@
 //
 //  ContentView.swift
-//  Calc
+//  Calculator
 //
-//  Created by nbk on 17/03/2024.
+//  Created by dvp on 14/03/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+       Home()
     }
 }
 
